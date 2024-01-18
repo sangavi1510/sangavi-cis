1e415a0ef761f2de63160cfd360563bbabc4f637
sangavi1510 <sangavik1510@gmail.com>
2024-01-17 20:14:00 -0600

    added new files

---
+++ javafiles/Assignment3/MainClass.java
@@ -0,0 +1,15 @@
+package Assignment3;
+
+public class MainClass 
+{
+	public static void main(String[] args) {
+		System.out.println(CodeOnStatic.i);
+		
+		CodeOnStatic.i=25;
+		System.out.println(CodeOnStatic.m1());
+		
+	}
+	
+	
+
+}
---
+++ javafiles/Assignment3/Parent.java
@@ -0,0 +1,12 @@
+package Assignment3;
+
+public class Parent {
+	int p;
+
+	public Parent(int p) {
+		this.p = p;
+	}
+
+	
+
+}
