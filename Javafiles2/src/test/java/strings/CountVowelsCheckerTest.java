02009d3c578ad5f80771d250320b101292a6da12
sangavi1510 <sangavik1510@gmail.com>
2024-01-23 15:28:00 -0600

    refactor

---
+++ Javafiles2/src/test/java/strings/CountVowelsCheckerTest.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+import strings.AnagramChecker;
+import strings.CountVowels;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+
+public class CountVowelsCheckerTest {
+
+    @Test
+    public void test1(){
+//        boolean cond = CountVowels.countVowels("abc");
+        assertEquals(1,CountVowels.countVowels("abc"));
+    }
+    @Test
+    public void test2(){
+//        boolean cond = CountVowels.countVowels("abc");
+        assertEquals(5,CountVowels.countVowels("aeiou"));
+    }
+
+    @Test
+    public void test3(){
+//        boolean cond = CountVowels.countVowels("abc");
+        assertEquals(0,CountVowels.countVowels(""));
+    }
+
+}
